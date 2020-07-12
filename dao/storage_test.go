4af092b0afd51595351147535795976c172b56df
leewei05 <B10202013@mail.ntust.edu.tw>
2020-07-12 15:35:57 +0800

    Test Check Exists

--- dao/storage_test.go
+++ dao/storage_test.go
@@ -8,6 +8,12 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+var (
+	testBucket         = "lee-image-test"
+	testImageNotExists = "dog.png"
+	testImageExists    = "cat.jpg"
+)
+
 type storageTestSuite struct {
 	suite.Suite
 	impl *storageDao
@@ -24,4 +30,14 @@ func (s *storageTestSuite) SetupTest() {
 	gcs := NewStorage(client)
 
 	s.impl = gcs.(*storageDao)
+}
+
+func (s *storageTestSuite) TestCheckExists() {
+	b, err := s.impl.CheckExists(testBucket, testImageExists)
+	s.NoError(err)
+	s.Equal(true, b)
+
+	b, err = s.impl.CheckExists(testBucket, testImageNotExists)
+	s.NoError(err)
+	s.Equal(false, b)
 }
