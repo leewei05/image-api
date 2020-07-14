11fe729d0e681e2336308e242e752d3157b1a916
leewei05 <B10202013@mail.ntust.edu.tw>
2020-07-14 18:40:20 +0800

    new rest

--- rest/rest.go
+++ rest/rest.go
@@ -3,43 +3,48 @@ package rest
 import (
 	"net/http"
 
+	"cloud.google.com/go/storage"
 	"github.com/go-redis/redis"
 	"github.com/jinzhu/gorm"
+	"github.com/leewei05/image-api/core"
 )
 
 // Rest is a struct
-type Rest struct {
+type rest struct {
 	db  *gorm.DB
 	rdb *redis.Client
+	gcs *storage.Client
 }
 
 // NewRest is a rest
 func NewRest(
 	db *gorm.DB,
 	rdb *redis.Client,
-) *Rest {
-	return &Rest{
+	gcs *storage.Client,
+) core.Rest {
+	return &rest{
 		db:  db,
 		rdb: rdb,
+		gcs: gcs,
 	}
 }
 
 // GetProduct is
-func (ri *Rest) GetProduct(w http.ResponseWriter, r *http.Request) {
+func (ri *rest) GetProduct(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello"))
 }
 
 // CreateProduct is
-func (ri *Rest) CreateProduct(w http.ResponseWriter, r *http.Request) {
+func (ri *rest) CreateProduct(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello"))
 }
 
 // UpdateProduct is
-func (ri *Rest) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+func (ri *rest) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello"))
 }
 
 // DeleteProduct is
-func (ri *Rest) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+func (ri *rest) DeleteProduct(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello"))
 }
